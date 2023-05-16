069215c377aa185af5fb3d24b0942e2f1b0d46b6
Joel Merino <joel@vauxoo.com>
2023-05-16 15:54:37 -0600

    [IMP] estate: computed fields and onchanges have been added to the models
    Added computed fields in the models:
    - estate_property
    - estate_property_offer
    
    Added an onchanges in the model:
    - estate_property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyModel(models.Model):
     _name = 'estate.property'
@@ -25,4 +25,28 @@ class EstatePropertyModel(models.Model):
     salesman_id = fields.Many2one("res.users", string="Salesman", default=lambda self: self.env.user)
     tag_ids = fields.Many2many("estate.property.tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
+    total_area = fields.Float(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price", store=True, default=0.0)
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for record in self:
+          if record.offer_ids:
+            record.best_price = max(record.offer_ids.mapped('price'))
+          else:
+            record.best_price = 0.0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
 
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,3 +1,4 @@
+from datetime import timedelta, datetime
 from odoo import api, fields, models
 
 class EstatePropertyOfferModel(models.Model):
@@ -9,5 +10,15 @@ class EstatePropertyOfferModel(models.Model):
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy="false")
     partner_id = fields.Many2one("res.partner", required=False)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", store=True, inverse="_inverse_date_deadline")
 
-    
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = datetime.today().date() + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = abs((record.date_deadline - datetime.today().date()).days)
+
