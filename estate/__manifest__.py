6a088c21a09a559027a129295e982af43640410a
Joel Merino <joel@vauxoo.com>
2023-05-18 19:58:16 -0600

    [FIX] estate: lints

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,21 +1,20 @@
 {
-    'name': 'My first module',
-    'summary': 'Real estate module of the technical course getting started with odoo',
-    'version': '16.0.1.0.0',
-    'author': 'Vauxoo',
-    'category': 'Installer',
-    'depends': [
-        'base',
+    "name": "My first module",
+    "summary": "Real estate module of the technical course getting started with odoo",
+    "version": "16.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Installer",
+    "depends": [
+        "base",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/estate_property_views.xml',
-        'views/estate_property_type_views.xml',
-        'views/estate_property_tag_views.xml',
-        'views/estate_property_offer_views.xml',
-        'views/estate_menus.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_type_views.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_property_offer_views.xml",
+        "views/estate_menus.xml",
     ],
     "application": True,
     "installable": True,
 }
-
