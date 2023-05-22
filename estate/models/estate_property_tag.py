b2c98b1597ab7095a7820bd4907ace9e9686fd56
Joel Merino <joel@vauxoo.com>
2023-05-22 00:33:02 -0600

    [ADD] estate: add the sprinkles added - inline list views - field widgets - model ordering -conditional display of buttons. - optional field - some decorations - default filter - stat button to property type

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -4,7 +4,9 @@ from odoo import fields, models
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
     _description = "Estate property tag Model"
+    _order = "name"
 
     name = fields.Char(required=True)
+    color = fields.Integer()
 
     _sql_constraints = [("unique_name", "UNIQUE(name)", "The tag name must be unique")]
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,10 +1,30 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Estate property type Model"
 
-    name = fields.Char(required=True)
+    name = fields.Char(string="Nombre del tupo", required=True)
+    property_ids = fields.One2many("estate.property", "property_type_id")
+    sequence = fields.Integer()
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id")
+    offer_count = fields.Integer(compute="_compute_offer_count")
 
     _sql_constraints = [("unique_name", "UNIQUE(name)", "The name of the property type must be unique")]
+
+    @api.depends("offer_ids")
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
+
+    def action_view_offers(self):
+        self.ensure_one()
+        return {
+            "name": "Offers",
+            "type": "ir.actions.act_window",
+            "view_mode": "tree",
+            "res_model": "estate.property.offer",
+            "domain": [("property_type_id", "=", self.id)],
+            "context": "{'create': False}",
+        }
