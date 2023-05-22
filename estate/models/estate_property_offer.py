7867ebba7534d7f9e3967b05f7ba0ff478f058ab
Joel Merino <joel@vauxoo.com>
2023-05-22 00:33:02 -0600

    [ADD] estate: add the sprinkles added - inline list views - field widgets - model ordering -conditional display of buttons. - optional field - some decorations - default filter - stat button to property type

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -15,6 +15,7 @@ class EstatePropertyOffer(models.Model):
     property_id = fields.Many2one("estate.property", required=True)
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(compute="_compute_date_deadline", store=True, inverse="_inverse_date_deadline")
+    property_type_id = fields.Many2one("estate.property.type", related="property_id.property_type_id", store=True)
 
     @api.depends("create_date", "validity")
     def _compute_date_deadline(self):
