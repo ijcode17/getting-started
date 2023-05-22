c093319f053416c7b3e0c1fe15e62e6844cbeece
Joel Merino <joel@vauxoo.com>
2023-05-22 13:02:32 -0600

    [ADD] estate_account: add new module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,14 @@
+{
+  'name': 'Estate Account',
+  'summary': 'Estate Account Module of the technical course getting started with odoo',
+  'version': '16.0.1.0.0',
+  'author': 'Vauxoo',
+  'category': 'Installer',
+  'depends': [
+    'estate',
+    'account',
+  ],
+  'application': True,
+  'installable': True,
+}
+
---
+++ estate_account/models/estate_property.py
@@ -0,0 +1,33 @@
+from odoo import Command, models
+
+class EstateProperty(models.Model):
+    _inherit = "estate.property"
+
+    def action_sold(self):
+        accepted_offer = self.offer_ids.find_accepted_offer()
+        journal = self.env['account.journal'].search([('type', '=', 'sale')], limit=1)
+        self.env['account.move'].create({
+          'partner_id': accepted_offer.partner_id.id,
+          'move_type': 'out_invoice',
+          'journal_id': journal.id,
+          'invoice_line_ids': [
+              Command.create({
+                  'name': self.name,
+                  'quantity': 1,
+                  'price_unit': self.selling_price,
+              }),
+              Command.create({
+                  'name': 'Administrative Fees',
+                  'quantity': 1,
+                  'price_unit': 100.0,
+              }),
+              Command.create({
+                  'name': 'Sales Commission',
+                  'quantity': 1,
+                  'price_unit': self.selling_price * 0.06,
+              }),
+          ],
+        })
+        res = super().action_sold()
+        print("Executing action_sold() from estate_account")
+        return res
