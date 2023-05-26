6effe54402ca2339de7f80d9ef3f1fd70b49b930
Joel Merino <joel@vauxoo.com>
2023-05-26 16:06:53 -0600

    [ADD] estate: add restrict access to data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,12 +3,13 @@
     "summary": "Real estate module of the technical course getting started with odoo",
     "version": "16.0.1.0.0",
     "author": "Vauxoo",
-    "category": "Installer",
+    "category": "Real Estate/Brokerage",
     "depends": [
         "base",
     ],
     "data": [
         "security/ir.model.access.csv",
+        "security/security.xml",
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
         "views/estate_property_tag_views.xml",
