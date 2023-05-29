7fd4344fa65368d66f4b84c7c7fb84424b92d61b
Joel Merino <joel@vauxoo.com>
2023-05-29 12:42:05 -0600

    [ADD]: estate: add pdf reports

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -1,14 +1,13 @@
 {
-  'name': 'Estate Account',
-  'summary': 'Estate Account Module of the technical course getting started with odoo',
-  'version': '16.0.1.0.0',
-  'author': 'Vauxoo',
-  'category': 'Installer',
-  'depends': [
-    'estate',
-    'account',
-  ],
-  'application': True,
-  'installable': True,
+    "name": "Estate Account",
+    "summary": "Estate Account Module of the technical course getting started with odoo",
+    "version": "16.0.1.0.0",
+    "author": "Vauxoo",
+    "category": "Installer",
+    "depends": [
+        "estate",
+        "account",
+    ],
+    "application": True,
+    "installable": True,
 }
-
